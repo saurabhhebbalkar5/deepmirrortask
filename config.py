36ac975313a4fc82339df9f78373351dadaf91a2
Saurabh Hebbalkar <55213356+saurabhhebbalkar5@users.noreply.github.com>
2022-11-19 17:57:00 +0530

    First Commit

---
+++ config.py
@@ -0,0 +1,23 @@
+from configparser import ConfigParser
+
+def config(filename='aws_database.ini', section='postgresql'):
+    '''
+    Function to fetch parameters/credentials from aws_database.ini config file to conect to DB and return dictionary 
+    '''
+    
+    # creating a parser
+    parser = ConfigParser()
+    
+    # reading the config file
+    parser.read(filename)
+  
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+  
+    return db
